f85176d7cb395af2831f4acab80ddcdd32b487c7
this-is-matt <82925612+this-is-matt@users.noreply.github.com>
2021-10-30 16:18:25 -0600

    Gallery Less Time Since

---
+++ lesson7/js/LastVisit.js
@@ -0,0 +1,8 @@
+// need Current time
+// need if to det. if first visit or last visit
+//f to do calculation
+//f to update local storage
+//const to det. mil to day conversion
+
+const now = new Date.now();
+const milToDay = 86400000;
--- lesson7/js/lazyLoad.js
+++ lesson7/js/lazyLoad.js
@@ -27,3 +27,4 @@ if ("IntersectionObserver" in window) {
     loadImages(img);
   });
 }
+
